bca54ffcf8e33cc3f390f8156551496e22ad9a69
Teresa Garcia <41209332+aleck91@users.noreply.github.com>
2024-06-24 13:11:43 -0600

    back

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+import express from "express"
+import mysql2 from "mysql2"
+
+const app = express()
+
+const db = mysql2.createConnection({
+    host: "localhost",
+    user:"root",
+    password:"contrasena",
+    database:"plantitas"
+})
+
+app.get("/", (req,res)=>{
+    res.json("hello this is the backend")
+})
+
+app.get("/plantas", (req,res)=>{
+    const query = "SELECT * FROM plantitas"
+    db.query(query,(err,data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+app.listen(8800, ()=>{
+    console.log("Connected to backend")
+}) 
---
+++ src/pages/Arbol.js
@@ -0,0 +1,12 @@
+import react from 'react'
+import { DiVim } from 'react-icons/di'
+
+const Arbol = () => {
+    return(
+        <div>
+            Arbol
+        </div>
+    )
+}
+
+export default Arbol
