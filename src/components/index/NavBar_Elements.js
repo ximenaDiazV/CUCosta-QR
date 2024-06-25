63ecc42970e1c4e4883103dac0f5af79b259f83e
ximenaDiazV <ximdiva@gmail.com>
2024-06-25 14:37:06 -0600

    Diseño de Login y funcionalidades.
    Se diseñó el login, y también se corrigió el hecho de que se cierre tanto el dropdown menu como la ventana del login si se presiona fuera de estos, mejorando un poco la funcionalidad.

--- src/components/index/NavBar_Elements.js
+++ src/components/index/NavBar_Elements.js
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 import { NavLink as Link } from "react-router-dom";
 
+//BARRA DEL NAVEGADOR.
 export const Nav = styled.nav`
     background: #FFFFFF;
-    height: 80px;
+    height: 90px;
     display: flex;
     justify-content: space-between;
     padding: 0.5rem calc((100vw - 1000px) / 2);
@@ -25,67 +26,28 @@ export const NavLink = styled(Link)`
     }
 `;
 
-export const Logo = styled(Link)`
-    display: flex;
-    align-items: center;
-    margin-right: 100px;
-    margin-left: -180px;
-    img {
-        height: 80px;
-        width: auto;
-    }
-`;
-
-
-export const SearchBarWrapper = styled.div`
-    position: relative;
-    display: flex;
-    align-items: center;
-`;
-
-export const SearchIcon = styled.div`
-    position: absolute;
-    right: 10px;
-    top: 50%;
-    transform: translateY(-50%);
-    color: #11111; /* Color del icono de búsqueda */
-    cursor: pointer;
-    background-color: #83C5BE;
-    border-radius: 50%;
-    padding: 7px 12px;
-`;
-
-export const SearchBar = styled.input`
-    width: 500px;
-    height: 42px;
-    margin-left: -450px; 
-    border: 2.5px solid black;
-    border-radius: 30px;
-    padding-left: 15px;
-    padding-right: 60px;
-`;
-
 export const NavMenu = styled.div`
     display: flex;
     align-items: center;
 `;
 
 export const NavBtnLink = styled(Link)`
-    border-radius: 20px;
+    border-radius: 25px;
     background: #83C5BE;
-    padding: 17px 30px;
+    padding: 18px 36px;
     color: #111111;
+    font-size: 23px;
     font-weight: bold;
     border: none;
     outline: none;
     cursor: pointer;
     transition: all 0.2s ease-in-out;
     text-decoration: none;
-    margin-left: 60px;
+    margin-left: 110px;
     margin-right: -200px;
     &:hover {
-        background: #4E8098;
-        color: #010606;
+        background: #006D77;
+        color: #FFFF;
         font-weight: bold;
     }
     &:active,
@@ -97,22 +59,102 @@ export const NavBtnLink = styled(Link)`
 `;
 
 
+// LOGO DE LA BARRA DEL NAVEGADOR - CUCOSTA.
+export const Logo = styled(Link)`
+    display: flex;
+    align-items: center;
+    margin-right: 100px;
+    margin-left: -340px;
+    img {
+        height: 90px;
+        width: auto;
+    }
+`;
+
+// LOGO EN LOGIN - CUCOSTA.
+export const Logo2 = styled(Link)`
+    display: flex;
+    margin-left: 100px;
+    margin-top: 10px;
+    cursor: pointer;
+    img {
+        height: 80px;
+        width: auto;
+    }
+`;
+
+// ICONOS ---------------------------------
+export const IconClose = styled(Link)`
+    display: flex;
+    margin-left: 373px;
+    margin-top: -90px;
+    img {
+        height: 25px;
+        width: auto;
+    }
+`;
+
+export const Icon = styled(Link)`
+    margin-left: -30px; 
+    margin-top: 9px;
+    padding: 4px 4px;
+`;
+
+// BARRA DE BUSQUEDA ------------------------------
+export const SearchBarWrapper = styled.div`
+    position: relative;
+    display: flex;
+    align-items: center;
+`;
+
+export const SearchIcon = styled.div`
+    position: absolute;
+    right: 10px;
+    top: 28%;
+    img {
+        height: 28px;
+        width: auto;
+    }
+    transform: translateY(-20%);
+    color: #11111; /* Color del icono de búsqueda */
+    cursor: pointer;
+    background-color: #83C5BE;
+    border-radius: 50%;
+    padding: 5px 8px;
+`;
+
+export const SearchBar = styled.input`
+    width: 500px;
+    height: 48px;
+    margin-left: -450px; 
+    border: 3px solid black;
+    border-radius: 30px;
+    padding-left: 15px;
+    padding-right: 60px;
+    font-size: 19px;
+`;
+
+
+// DROPDOWN - BOTÓN EXPLORAR -----------------------
 export const DropdownMenu = styled.div`
     position: absolute;
-    top: 85px;
-    margin-left: 0px;
+    top: 100px;
+    margin-left: 80px;
     background: #83C5BE;
     border-radius: 20px;
-    padding: 10px 10px;
+    padding: 30px 30px;
     box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
 `;
 
 export const DropdownItem = styled(Link)`
     color: #000000; /* Color negro */
     font-weight: bold; /* Texto en negritas */
-    padding: 10px 20px;
+    padding: 10px 25px;
+    margin-left: -5px;
+    margin-right: -5px;
     text-decoration: none;
     display: block;
+    font-size: 20px;
     text-align: right;
     &:hover {
         background: #0B5351;
@@ -122,24 +164,22 @@ export const DropdownItem = styled(Link)`
     }
 `;
 
-export const SignInBtn = styled(Link)`
-    border-radius: 20px;
-    bottom: -100px;
-    background: #092327;
-    padding: 10px 40px;
-    color: #FFFFFF;
-    font-weight: bold;
-    border: none;
-    outline: none;
-    cursor: pointer;
-    transition: all 0.2s ease-in-out;
-    text-decoration: none;
-    margin-left: 80px;
-    margin-right: -200px;
-    padding-bottom: -10px;
+
+// CSS PARA LOGIN -------------------------
+export const LoginWrapper = styled.div`
+    background: #83C5BE;
+    border-radius: 40px;
+    width: 310px;
+    height: 300px;
+    margin-left: 55px;
+    margin-top: -345px;
 `;
 
-export const ShadowBoxLoginForm = styled.div`
-    border-radius: 20px;
+export const BoxLoginWrapper = styled.div`
     background: #092327;
+    border-radius: 40px;
+    width: 320px;
+    height: 300px;
+    margin-left: 30px;
+    margin-top: 40px;
 `;
