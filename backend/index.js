bae872df6e6e441317eeda5c5317c7d7b7af2074
Teresa Garcia <41209332+aleck91@users.noreply.github.com>
2024-06-26 10:50:10 -0600

    pruebas backend login
    conexion a las credenciales de mysql en index.js a traves de la url conect desde react /arbol

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,40 @@
 import express from "express"
 import mysql2 from "mysql2"
 import cors from "cors"
+import { useState } from "react"
 
 const app = express()
 
 const db = mysql2.createConnection({
     host: "localhost",
-    user:"root",
-    password:"contrasena",
+    user:"read",
+    password:"CUCosta.2024#qr",
     database:"plantitas"
 })
-
+ 
 app.use(express.json())
 app.use(cors())
 
+app.post("/conect", (req,res)=>{
+    console.log(req.body.user)
+    console.log(req.body.pass)
+    const db2 = mysql2.createConnection({
+        host: "localhost",
+        user:req.body.user,
+        password:req.body.pass,
+        database:"plantitas"
+    })
+
+    db2.connect((err) => {
+        if(err) {
+            console.error('Error connecting mysql: '+err.stack);
+            return;
+        }
+        console.log('Connected to mysql as ID ' + db2.threadId);
+    })
+
+
+})
 
 app.get("/", (req,res)=>{
     res.json("hello this is the backend")
@@ -25,6 +46,11 @@ app.get("/plantas", (req,res)=>{
         if(err) return res.json(err)
         return res.json(data)
     })
+    {/*const query = "INSERT INTO plantitas (`idplantitas`, `name`) VALUES ('4', 'sal')";
+    db.query(query,(err,data)=>{
+        if(err) return res.json(err)
+        return res.json("Ingreso datos")
+    })*/}
 })
 
 app.listen(8800, ()=>{
--- src/pages/Arbol.js
+++ src/pages/Arbol.js
@@ -3,21 +3,19 @@ import { DiVim } from 'react-icons/di'
 import axios from 'axios'
 const Arbol = () => {
 
-const [plantas, setplantas] = useState([])
-useEffect(()=>{
-    const fecthAllplantas = async ()=>{
-        try{
-            const res = await axios.get("http://localhost:8800/plantas")
-            setplantas(res.data);
-        }catch(err){
-            console.log(err)
+    const [plantas, setplantas] = useState([])
+    useEffect(()=>{
+        const fecthAllplantas = async ()=>{
+            try{
+                const res = await axios.get("http://localhost:8800/plantas")
+                setplantas(res.data);
+            }catch(err){
+                console.log(err)
+            }
         }
-    }
-    fecthAllplantas()
-},[])
-
-
-    return(
+        fecthAllplantas()
+    },[])
+    {/* traer datos mysql
     <div>
     {plantas.map(planta=>(
         <div>
@@ -29,6 +27,47 @@ useEffect(()=>{
             </h2>
         </div>
     ))}
+    </div>*/}
+
+    const [user, setUser] = useState({
+        user: "",
+        pass: "",
+    });
+
+    const handleChange=(e) =>{
+        setUser(prev=>({...prev, [e.target.name]: e.target.value}))
+    };
+    console.log(user)
+
+    const handleClick = async e =>{
+        e.preventDefault()
+        try{
+            await axios.post("http://localhost:8800/conect", user)
+        }catch(err){
+            console.log(err)
+        }
+    }
+
+    return(
+    <div>
+        <div className='form'>
+            <h1>login</h1>
+            <input type="text" placeholder='user' onChange={handleChange} name="user"/>
+            <input type="text" placeholder='pass' onChange={handleChange} name="pass"/>
+            <button onClick={handleClick} >login</button>
+        </div>
+        <div>
+        {plantas.map(planta=>(
+            <div>
+                <h2>
+                    {planta.idplanta}
+                </h2>
+                <h2>
+                    {planta.name}
+                </h2>
+            </div>
+        ))}
+        </div>
     </div>
     )
 }
